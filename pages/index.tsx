6b9e6a638f130cd30f8e9ae64846e73022cb10f0
EgaAprianto <egaaprianto04@gmail.com>
2023-12-22 06:59:52 +0700

    [TUGAS] Hari-9

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Layout from "@/layouts";
+import { useEffect } from "react";
 
 export default function Main() {
+  useEffect(() => {
+    fetch("/api/hello")
+    .then(res => res.json())
+      .then((res) => console.log("response =>", res))
+      .catch((err) => console.log("err =>", err));
+  }, []);
+
   return (
     <div>
-      <Layout metaTitle="Home" metaDescription=" Semua Informasi ini mengenai fitur-fitur yang tersedia pada website">
+      <Layout
+        metaTitle="Home"
+        metaDescription=" Semua Informasi ini mengenai fitur-fitur yang tersedia pada website"
+      >
         <p className="text-6xl p-32">Home</p>
       </Layout>
     </div>
