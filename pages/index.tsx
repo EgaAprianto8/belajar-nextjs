5851b95db4ca395fac778c00d8f844ec20cbdfbc
EgaAprianto <egaaprianto04@gmail.com>
2023-12-23 07:23:38 +0700

    [TUGAS] hari-10

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,31 @@
-import Layout from "@/layouts";
 import { useEffect } from "react";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+
+const LayoutComponent = dynamic(() => import("@/layouts"));
 
 export default function Main() {
   useEffect(() => {
     fetch("/api/hello")
-    .then(res => res.json())
+      .then((res) => res.json())
       .then((res) => console.log("response =>", res))
       .catch((err) => console.log("err =>", err));
   }, []);
 
   return (
     <div>
-      <Layout
+      <LayoutComponent
         metaTitle="Home"
         metaDescription=" Semua Informasi ini mengenai fitur-fitur yang tersedia pada website"
       >
-        <p className="text-6xl p-32">Home</p>
-      </Layout>
+        <div className="flex flex-row">
+          <p className="text-6xl p-32">Home</p>
+          <Image
+            src="/next.png" width={400} height={400} 
+            alt="next image" className="object-cover"
+          />
+        </div>
+      </LayoutComponent>
     </div>
   );
 }
